016fde7947f0946d2d622bc4a174c62e70346621
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2023-12-22 23:23:33 +0000

    Make database more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,31 +9,58 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error('something bad has happened!', error);
+    // console.error('something bad has happened!', error.stack);
+  } finally {
+    await client.end();
+  }
 }
 
-async function postgres_info() {
-  const q_version = await query("SELECT version() as version;");
-  const version = q_version.rows[0].version;
+async function database_info() {
+  // const q_version = await query("SELECT version();");
+  const q_version = await query("SHOW server_version;");
+  const version = q_version.rows[0].server_version;
   
   const q_max_connections = await query("SHOW max_connections;");
   const max_connections = q_max_connections.rows[0].max_connections;
 
-  const q_used_connections = await query("SELECT count(*) AS used_connections FROM pg_stat_activity;");
-  const used_connections = q_used_connections.rows[0].used_connections;
-  
-  const result = {
-    postgres_version: version,
-    postgres_max_connections: max_connections,
-    postgres_used_connections: used_connections,
+  // const databaseName = "local_db";
+  const databaseName = process.env.POSTGRES_DB;
+  // const query_used_connections =
+  //    "SELECT count(*) AS used_connections FROM pg_stat_activity;";
+  // const query_used_connections =
+  //    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';";
+  // "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
+  // "SELECT count(*)::int FROM pg_stat_activity WHERE datname = '" + databaseName + "';"
+  // console.log(`q_used_connections = ${q_used_connections}`);
+/*
+  const query_used_connections =
+    `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`
+*/
+  const query_used_connections =
+  {
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
   }
+  const q_used_connections = await query(query_used_connections);
+  // console.log(q_used_connections);
+  const used_connections = q_used_connections.rows[0].count;
+  
+  const result = { 
+      // vendor: "postgresql",
+      version: version,
+      max_connections: max_connections,
+      used_connections: used_connections,
+  };
   
   return result;
 }
 
 export default {
   query: query,
-  postgres_info: postgres_info
+  database_info: database_info
 };
